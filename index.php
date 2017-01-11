748eb624fa5dbb5546fa3b08b2fc13f8571a6466
wabrown <crunchymail27@gmail.com>
2017-01-10 16:50:31 -0800

    added sorting

--- index.php
+++ index.php
@@ -31,7 +31,9 @@
 			$second->add_grade(80);
 			$second->add_grade(50);
 			$students['a456'] = $second;
-
+			
+			ksort($students); 
+			
 			foreach($students as $student)
 				echo $student->toString();
 		?>

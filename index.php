a28100cad4477b8ad846e50aac850f77fcaf2fd7
wabrown <crunchymail27@gmail.com>
2017-01-10 16:32:27 -0800

    Added homepage

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>a title</title>
+	</head>
+	<body>
+	
+		<?php
+			echo 'Hello World:';
+		?>
+	</body>
+</html>
